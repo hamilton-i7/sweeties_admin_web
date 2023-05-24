20fa043c492d47ab7a67b68e6d3d4312371ad2f9
= <=>
2023-05-24 13:42:18 -0500

    Create Category model

--- sweeties_admin_backend/api/models.py
+++ sweeties_admin_backend/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from utils.abstract_models import UUIDModel, TimestampModel
+from utils import constants
 
-# Create your models here.
+
+class Category(UUIDModel, TimestampModel):
+    name = models.CharField(max_length=constants.NAME_MAX_LENGTH)
+    active = models.BooleanField(default=False)
