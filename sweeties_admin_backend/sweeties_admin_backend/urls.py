6a25df7f5dc1380ec54834eae7453ccee385c953
Juan Hamilton <hedwards507@gmail.com>
2023-05-25 00:15:31 -0500

    Add serializers and ModelViewSet for Products

--- sweeties_admin_backend/sweeties_admin_backend/urls.py
+++ sweeties_admin_backend/sweeties_admin_backend/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from api.views import CategoryViewSet
+from api.views import CategoryViewSet, ProductViewSet
 from django.urls import path, include
 
 router = DefaultRouter()
 router.register(r"categories", CategoryViewSet, basename="category")
+router.register(r"products", ProductViewSet, basename="product")
 
 urlpatterns = [path("api/", include(list(router.urls)))]
