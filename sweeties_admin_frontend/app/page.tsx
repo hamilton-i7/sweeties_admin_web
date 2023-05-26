37892337618761266fc097af443640c2f0f9de7d
= <=>
2023-05-26 10:59:30 -0500

    Create Subscribers component

--- sweeties_admin_frontend/app/page.tsx
+++ sweeties_admin_frontend/app/page.tsx
@@ -1,9 +1,14 @@
-import TextButton from '../components/common/button/TextButton'
+import TextButton from '@/components/common/button/TextButton'
+import Subscribers from '@/components/Subscribers'
 
 export default function Home() {
   return (
-    <main className='flex flex-col'>
-      <TextButton label='Ver actividad reciente' className='self-end mr-4' />
+    <main className='flex flex-col text-on-background'>
+      <TextButton
+        label='Ver actividad reciente'
+        className='self-end mr-4 mb-6'
+      />
+      <Subscribers />
     </main>
   )
 }
