4a7e87e579644d259c976ecd1d5f48d9b39cf16c
= <=>
2023-05-29 08:39:50 -0500

    Add animation to SideBarWithNac

--- sweeties_admin_frontend/components/common/SideBarWithNav.tsx
+++ sweeties_admin_frontend/components/common/SideBarWithNav.tsx
@@ -26,7 +26,10 @@ export default function SideBarWithNav({ title }: SideBarWithNavProps) {
           </Dialog.Trigger>
           <Dialog.Portal>
             <Dialog.Overlay className='bg-scrim fixed top-0 left-0 w-full h-screen' />
-            <Dialog.Content asChild>
+            <Dialog.Content
+              asChild
+              className='data-[state=open]:animate-slide-in'
+            >
               <aside className='w-80 h-screen overflow-y-auto fixed top-0 left-0 rounded-r-[2rem] bg-background py-6 px-4 flex flex-col outline-none'>
                 <header className='flex items-center gap-x-3 mb-6 px-3'>
                   <Image
@@ -36,7 +39,8 @@ export default function SideBarWithNav({ title }: SideBarWithNavProps) {
                   />
                   <h3
                     id='main-navigation-label'
-                    className='body-l text-on-surface-variant'>
+                    className='body-l text-on-surface-variant'
+                  >
                     Sweeties672
                   </h3>
                 </header>
@@ -83,7 +87,8 @@ function SideBarItem({ iconName, label, active = false }: SideBarItemProps) {
     <li className='gap-x-3 w-full h-14'>
       <a
         href='#'
-        className={`h-14 rounded-[6.25rem] pl-4 pr-6 flex items-center gap-x-3 ${itemColors} focus:outline-none`}>
+        className={`h-14 rounded-[6.25rem] pl-4 pr-6 flex items-center gap-x-3 ${itemColors} focus:outline-none`}
+      >
         <span className='material-icons-round'>{iconName}</span>
         <p className='label-l'>{label}</p>
       </a>
--- sweeties_admin_frontend/tailwind.config.js
+++ sweeties_admin_frontend/tailwind.config.js
@@ -7,10 +7,18 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        slide: {
+          from: {
+            transform: 'translateX(-20rem)',
+          },
+          to: {
+            transform: 'translateX(0)',
+          },
+        },
+      },
+      animation: {
+        'slide-in': 'slide 250ms ease-in',
       },
     },
     screens: {
