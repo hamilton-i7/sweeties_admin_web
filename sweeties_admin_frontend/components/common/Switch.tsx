237f23e89ba3d60bcef0b8b2c00cb498f86fa415
= <=>
2023-06-02 10:25:56 -0500

    Add focus feedback to Switch

--- sweeties_admin_frontend/components/common/Switch.tsx
+++ sweeties_admin_frontend/components/common/Switch.tsx
@@ -7,9 +7,9 @@ export default function Switch(props: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <RadixSwitch.Root
       {...props}
-      className='w-[3.25rem] h-8 bg-surface-variant data-[state=checked]:bg-secondary rounded-full relative outline-none border-[0.125rem] border-outline border-solid data-[state=checked]:border-none cursor-pointer'
+      className='group w-[3.25rem] h-8 bg-surface-variant data-[state=checked]:bg-secondary rounded-full relative outline-none border-[0.125rem] border-outline border-solid data-[state=checked]:border-none cursor-pointer'
     >
-      <RadixSwitch.Thumb className='block w-4 h-4 data-[state=checked]:w-6 data-[state=checked]:h-6 bg-outline data-[state=checked]:bg-on-secondary rounded-full transition-transform duration-100 translate-x-1 will-change-transform data-[state=checked]:translate-x-[1.5rem]' />
+      <RadixSwitch.Thumb className='block w-4 h-4 active:w-7 active:h-7 data-[state=checked]:w-6 data-[state=checked]:h-6 bg-outline data-[state=checked]:bg-on-secondary rounded-full transition-transform duration-100 translate-x-1 will-change-transform data-[state=checked]:translate-x-[1.5rem] group-focus:before:w-7 group-focus:before:h-7 group-focus:before:bg-scrim before:rounded-full before:overflow-hidden before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2' />
     </RadixSwitch.Root>
   )
 }
